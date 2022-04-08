d41b0af9056955b1e5f8f4a9d26b6562cc231058
Godschild01 <pelumikelvin@gmail.com>
2022-04-07 19:43:40 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *
+ *main - Entry Point
+ *
+ *Return: Always 0 (success)
+*/
+int main(void)
+{
+printf("Size of a char: %d bytes(s)\n", Sizeof(char));
+prinrf("Size of an int: %d bytes(s)\n", sizeof(int));
+printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+printf("size of a long long int: %d bytes(s)\n", sizeof(long long int));
+printf("size of float: %d bytes(s)\n", sizeof(float));
+return (0);
+}
