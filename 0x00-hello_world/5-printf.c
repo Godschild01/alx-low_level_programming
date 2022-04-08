1e861aafb3ff391b5c0187d81a79f5dd0ee586bb
Godschild01 <pelumikelvin@gmail.com>
2022-04-07 19:29:17 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
+
 #include <stdio.h>
-/**
- *main entry point
- *
- *Return:Always 0 (Success)
- */
+/** 
+ *main - Entry point
+ *My hello, printf task
+ *Return : return 1 after program execution
+*/
 int main(void)
 {
-prinf("with proper grammer, but the outcome is a piece of art, \n");
+printf("with proper grammar, but the outcome is a piece of art, \n");
 return (0);
 }
