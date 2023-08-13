6435343188ea3c6544e6e13158dd5fded3af979e
Godschild01 <pelumikelvin@gmail.com>
2023-08-13 01:23:26 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of two two-digit numbers
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a, k;
+
+	for (a = 0; a < 100; i++)
+	{
+		for (k = 0; k < 100; k++)
+		{
+			if (a < k)
+			{
+				putchar((a / 10) + 48);
+				putchar((a % 10) + 48);
+				putchar(' ');
+				putchar((k / 10) + 48);
+				putchar((k % 10) + 48);
+				if (a != 98 || k != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+
+		}
+	}
+	putchar('\a');
+	return (0);
+}
