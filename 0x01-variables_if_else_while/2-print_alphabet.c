ccbc611dec7701987c6b1d637ddc9fb19b4555d0
Godschild01 <pelumikelvin@gmail.com>
2023-08-13 00:04:48 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a-z[26] = "abcdefghijklmnopqrstuvwxyz";
+	int q;
+
+	for (q = 0; q < 26; q++)
+	{
+		putchar(alp[q]);
+	}
+	putchar('\n');
+	return (0);
+}
+
